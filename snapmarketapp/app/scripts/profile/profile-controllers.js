e58f908fe69737370dee1376168228933e004842
Joseph Lin <josephlin55555@gmail.com>
2015-05-25 15:34:57 -0700

    [modify] authentication to use native firebase commands

--- snapmarketapp/app/scripts/profile/profile-controllers.js
+++ snapmarketapp/app/scripts/profile/profile-controllers.js
@@ -1,32 +1,37 @@
 angular.module('profile.controllers', ['firebase'])
-.controller('LoginCtrl', function($scope, $state, $rootScope) {
+.controller('LoginCtrl', function($scope, $state) {
 
-  $rootScope.ref = new Firebase("https://snapmarket.firebaseio.com");
-  $rootScope.session = {};
+  var ref = new Firebase("https://snapmarket.firebaseio.com");
 
   $scope.auth = function() {
-    console.log('I am authenticating');
 
-    $rootScope.ref.authWithOAuthPopup("facebook", function(error, authData) {
+    //asks user to login using facebook acct
+    ref.authWithOAuthPopup("facebook", function(error, authData) {
       if (error) {
         console.log("Login Failed!", error);
       } else {
         console.log("Authenticated successfully with payload:", authData);
-        $rootScope.session = authData;
+        //redirect to profile page
         $state.go('tab.profile');
-      }
-    });
+      } //asks user for permission to grab facebook email
+    }, {scope: "email"});
   }
 })
-.controller('ProfileCtrl', function($scope, $rootScope, $state) {
+.controller('ProfileCtrl', function($scope, $state) {
 
-  if(!$rootScope.session) {
+  var ref = new Firebase("https://snapmarket.firebaseio.com");
+
+  //checks if current user is logged in; if not, redirects to login page
+  if(!ref.getAuth()) {
     $state.go('tab.login');
   }
 
   $scope.logout = function() {
-    $rootScope.ref.unauth();
-    $state.go('tab.login');
+    //ends current user session
+    ref.unauth(); 
+
+    //moves to login page
+    $state.go('tab.login'); 
   };
 
 
