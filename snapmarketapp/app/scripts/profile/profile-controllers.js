a1d801015293e013518c140d78077866da3cc8cf
Joseph Lin <josephlin55555@gmail.com>
2015-05-25 10:27:50 -0700

    [add] login page and authentication

--- snapmarketapp/app/scripts/profile/profile-controllers.js
+++ snapmarketapp/app/scripts/profile/profile-controllers.js
@@ -1,3 +1,9 @@
 angular.module('profile.controllers', ['firebase'])
-.controller('LoginCtrl', function($scope) {})
-.controller('ProfileCtrl', function($scope) {});
+.controller('LoginCtrl', function($scope) {
+  $scope.auth = function() {
+    console.log('I am authenticating');
+  };
+})
+.controller('ProfileCtrl', function($scope) {
+  
+});
