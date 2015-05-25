9dbf8d1a3a99f9d6cb6a8caa88c317ab7f45b848
Joseph Lin <josephlin55555@gmail.com>
2015-05-25 14:46:03 -0700

    [add] Facebook authentication

--- snapmarketapp/app/scripts/profile/profile-controllers.js
+++ snapmarketapp/app/scripts/profile/profile-controllers.js
@@ -1,11 +1,33 @@
 angular.module('profile.controllers', ['firebase'])
-.controller('LoginCtrl', function($scope, $state) {
+.controller('LoginCtrl', function($scope, $state, $rootScope) {
+
+  $rootScope.ref = new Firebase("https://snapmarket.firebaseio.com");
+  $rootScope.session = {};
+
   $scope.auth = function() {
     console.log('I am authenticating');
-    $state.go('tab.profile');
-  };
-  console.log('ready');
+
+    $rootScope.ref.authWithOAuthPopup("facebook", function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        console.log("Authenticated successfully with payload:", authData);
+        $rootScope.session = authData;
+        $state.go('tab.profile');
+      }
+    });
+  }
 })
-.controller('ProfileCtrl', function($scope) {
-  
+.controller('ProfileCtrl', function($scope, $rootScope, $state) {
+
+  if(!$rootScope.session) {
+    $state.go('tab.login');
+  }
+
+  $scope.logout = function() {
+    $rootScope.ref.unauth();
+    $state.go('tab.login');
+  };
+
+
 });
