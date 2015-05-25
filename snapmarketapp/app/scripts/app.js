f8d30bc847c1c0fee155e540df859f448bf8e5c4
dgrundfest <dave4@grundfest.com>
2015-05-25 15:06:03 -0700

    Added simple image capture. Must use ionic preview on device to use

--- snapmarketapp/app/scripts/app.js
+++ snapmarketapp/app/scripts/app.js
@@ -7,6 +7,10 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
+.config(function($compileProvider){
+  $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+})
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
--- snapmarketapp/app/scripts/sell/sell-controllers.js
+++ snapmarketapp/app/scripts/sell/sell-controllers.js
@@ -1,13 +1,17 @@
 angular.module('sell.controllers', [])
 .controller('SellCameraCtrl', function($scope , Camera) {
   $scope.getPhoto = function() {
-    Camera.getPicture(
-      { quality : 100,
-        sourceType : Camera.PictureSourceType.SAVEDPHOTOALBUM}
-    ).then(function(imageURI) {
+    Camera.getPicture().then(function(imageURI) {
       console.log(imageURI);
+      $scope.lastPhoto = imageURI;
     }, function(err) {
       console.err(err);
+    }, {
+      quality: 75,
+      sourceType : 0,
+      targetWidth: 320,
+      targetHeight: 320,
+      saveToPhotoAlbum: false
     });
   };
   $scope.dummyFunc = Camera.dummyFunc;
