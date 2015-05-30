821c2d04dc0e60be7b136153cae36fa71f4050c9
Vivek <vtumrukota@gmail.com>
2015-05-29 19:55:31 -0700

    [fix] Transaction now shows active Buy/Sell tab

--- snapmarketapp/app/scripts/transaction/transaction-controllers.js
+++ snapmarketapp/app/scripts/transaction/transaction-controllers.js
@@ -1,20 +1,11 @@
 angular.module('transaction.controllers', [])
 .controller('TransactionCtrl', function($scope, $state, Db) {
-  $scope.sellView = function(){
-    $state.go('tab.transaction.sellListings');
-    console.log('hits');
-  }
-  $scope.buyView = function(){
-    $state.go('tab.transaction.buyOffers');
-    console.log('hits');
-  }
- 
+
 })
 .controller('BuyOfferCtrl', function($scope) {})
 .controller('BuyTransactionChatCtrl', function($scope) {})
 
 .controller('SellListingsCtrl', function($scope, $state, Db) {
-  console.log(Db);
 
 
 })
