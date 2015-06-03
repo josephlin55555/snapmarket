31110c802caeebde5027da2464b3ff73405fd9e6
andyttran <andyttran@me.com>
2015-06-02 22:51:14 -0700

    [fix] Transaction states to display nested views for buy and sell. Back button required.

--- snapmarketapp/app/scripts/transaction/transaction-controllers.js
+++ snapmarketapp/app/scripts/transaction/transaction-controllers.js
@@ -1,44 +1,47 @@
 angular.module('transaction.controllers', [])
-.controller('TransactionCtrl', function($rootScope, $scope, $state, Db) {
-  //default to buy view once transaction tab is pressed
-  $state.go('tab.transaction.buyOffers'); 
-})
-
 //Buy Navigation controllers
 .controller('BuyOfferCtrl', function($rootScope, $scope, $state, $firebaseObject, Db) {
+  $rootScope.hideNav = true;
+  var buyOffers = Db.child('Users')
   $scope.dummy = [1,3,4,5,2,8];
  
   $scope.view = function(){
-    $state.go('tab.buy-transactionChat');
+    $state.go('tab.transaction.transactionChat');
   }
 
-  var buyOffers = Db.child('Users')
-
 })
-.controller('BuyTransactionChatCtrl', function($scope, Db) {})
-  //var buyOffer = Db.child('Users')
+.controller('BuyTransactionChatCtrl', function($rootScope, $scope, $state, Db) {
+  $rootScope.hideNav = false;
+  $rootScope.navTitle = "Item Chat";
+  console.log('buy transaction chat initiated')
+})
   
 
-//////////////////Sell Navigation controllers
-.controller('SellListingsCtrl', function($scope, $state, Db) {
+//Sell Navigation controllers
+.controller('SellListingsCtrl', function($rootScope, $scope, $state, Db) {
+  $rootScope.hideNav = true;
+  $rootScope.navTitle = "Listings";
   $scope.dummy = ['YO ITS COOL', 'FIRE STUFF', 'MAD SHIZZ'];
 
   $scope.viewList = function(){
-    console.log('shift states');
-    $state.go('tab.sellListingItems');
+    $state.go('tab.transaction.sellListingItems');
    }
 
 })
-.controller('SellListingItemsCtrl', function($scope, $state, Db) {
+
+.controller('SellListingItemsCtrl', function($rootScope, $scope, $state, Db) {
+  $rootScope.hideNav = false;
+  $rootScope.navTitle = "Items";
   $scope.dummy = ['LISTING 1', 'LISTING 2', 'LISTING 3'];
 
   $scope.viewItem = function(){
-    console.log('CHANGING OUR STATE');
-    $state.go('tab.sellListingItems.sellItemOffers');
+    $state.go('tab.transaction.sellItemOffers');
 
   }
 })
-.controller('SellItemOffersCtrl', function($scope, $state, Db) {
+.controller('SellItemOffersCtrl', function($rootScope, $scope, $state, Db) {
+  $rootScope.hideNav = false;
+  $rootScope.navTitle = "Item Chat";
   console.log('sellitemoffers is trying to load');
 })
 
