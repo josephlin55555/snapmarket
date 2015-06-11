3c08613eae43c24ced65c17eb698d55093bed1eb
Joseph Lin <josephlin55555@gmail.com>
2015-06-10 17:26:27 -0700

    [modify] merge conflicts for ENV

--- snapmarketapp/app/scripts/configuration.js
+++ snapmarketapp/app/scripts/configuration.js
@@ -2,10 +2,6 @@
 
  angular.module('config', [])
 
-<<<<<<< HEAD
 .constant('ENV', {name:'development',apiEndpoint:'http://dev.yoursite.com:10000/'})
 
 ;
-=======
-.constant('ENV', {name:'development',apiEndpoint:'http://dev.yoursite.com:10000/'});
->>>>>>> [modify] merged ENV conflicts
