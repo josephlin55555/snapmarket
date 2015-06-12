5a05032079b2f8c4287563f9c2011beec5c30340
andyttran <andyttran@me.com>
2015-06-11 21:50:40 -0700

    [merge] camera features from dave

--- snapmarketapp/app/scripts/configuration.js
+++ snapmarketapp/app/scripts/configuration.js
@@ -2,4 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'development',apiEndpoint:'http://dev.yoursite.com:10000/'});
+.constant('ENV', {name:'development',apiEndpoint:'http://dev.yoursite.com:10000/'})
+
+;
