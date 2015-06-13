b562fcd7916a0cb264d1867286c32be42948237a
andyttran <andyttran@me.com>
2015-06-13 14:38:03 -0700

    [add] moment for offer time and fixed image urls

--- snapmarketapp/app/scripts/transaction/transaction-controllers.js
+++ snapmarketapp/app/scripts/transaction/transaction-controllers.js
@@ -29,6 +29,7 @@ angular.module('transaction.controllers', [])
     offers.$loaded().then(function(){
       angular.forEach(offers, function(value, key) {
         if(offers[key].hasOwnProperty('buyer') && offers[key].buyer.uid === $rootScope.currentUser.toString()){
+         offers[key].moment = moment(offers[key].createdAt).fromNow();
          $scope.userOffers[key] = offers[key];
         }
       });
